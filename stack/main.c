ea54f944d6611caf1d75741b9fb2cb9874bf49de
per4 <per4@CompSci-06.local>
2022-05-10 12:08:01 -0700

    added tests for stack

--- stack/main.c
+++ stack/main.c
@@ -6,15 +6,27 @@ int main() {
 
     Stack s = stack_create();
 
+    printf("pushing 1, 2, 3 to the stack");
     stack_push(s, 1);
     stack_push(s, 2);
     stack_push(s, 3);
 
-    printf("the stack has %d elements\n", stack_size(s));
+    printf("the stack has %d elements, and has the following contents\n", stack_size(s));
+    stack_dump(s);
 
     printf("top of stack %d\n", stack_pop(s));
     printf("top of stack %d\n", stack_pop(s));
     printf("top of stack %d\n", stack_pop(s));
+
+    printf("stack peek %d\n", stack_peek(s));
+
+    printf("pushing 4, 5, 6 to the stack");
+    stack_push(s, 4);
+    stack_push(s, 5);
+    stack_push(s, 6);
+
+    printf("the stack has %d elements, and has the following contents\n", stack_size(s));
+    stack_dump(s);
 
     stack_destroy(s);
 
